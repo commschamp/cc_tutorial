e7191fdfca70b8c45c5ab5b75497746f7cada905
Alex Robenko <arobenko@gmail.com>
2020-05-13 08:34:53 +1000

    Added tutorial4 schema and code.

--- tutorials/tutorial4/src/ClientSession.cpp
+++ tutorials/tutorial4/src/ClientSession.cpp
@@ -16,10 +16,7 @@ namespace cc_tutorial
 void ClientSession::handle(Msg1& msg)
 {
     std::cout << "Received \"" << msg.doName() << "\" with ID=" << msg.doGetId() << '\n' <<
-        '\t' << msg.field_f1().name() << " = " << std::fixed << msg.field_f1().getScaled<float>() <<
-            " (orig = " << msg.field_f1().value() << ")\n" <<
-        '\t' << msg.field_f2().name() << " = " << std::fixed << msg.field_f2().getScaled<double>() <<
-            " (orig = " << msg.field_f2().value() << ")\n" <<
+
         std::endl;
 
     doNextStage();
@@ -27,43 +24,12 @@ void ClientSession::handle(Msg1& msg)
 
 void ClientSession::handle(Msg2& msg)
 {
-    // Compile time checks could be used before third party units conversions (like Boost.Units)
-    static_assert(comms::units::isSeconds<Msg2::Field_f1>(), "Unexpected units");
-    static_assert(comms::units::isMillimeters<Msg2::Field_f2>(), "Unexpected units");
-
     std::cout << "Received \"" << msg.doName() << "\" with ID=" << msg.doGetId() << '\n' <<
-        '\t' << msg.field_f1().name() << " = " << msg.field_f1().value() << '\n' <<
-            "\t\t= " << comms::units::getMicroseconds<std::uintmax_t>(msg.field_f1()) << " us\n" <<
-            "\t\t= " << comms::units::getMilliseconds<unsigned>(msg.field_f1()) << " ms\n" <<
-            "\t\t= " << comms::units::getSeconds<unsigned>(msg.field_f1()) << " s\n" <<
-            "\t\t= " << std::fixed << comms::units::getMinutes<float>(msg.field_f1()) << " min\n" <<
-            "\t\t= " << std::fixed << comms::units::getHours<double>(msg.field_f1()) << " h\n" <<
-        '\t' << msg.field_f2().name() << " = " << std::fixed << msg.field_f2().value() << '\n' <<
-            "\t\t= " << std::fixed << comms::units::getMicrometers<double>(msg.field_f2()) << " um\n" <<
-            "\t\t= " << std::fixed << comms::units::getMillimeters<double>(msg.field_f2()) << " mm\n" <<
-            "\t\t= " << std::fixed << comms::units::getMeters<double>(msg.field_f2()) << " m\n" <<
+
         std::endl;
 
     if (m_currentStage != CommsStage_Msg2) {
         std::cerr << "ERROR: Unexpected message received" << std::endl;
-        return;
-    }
-
-    doNextStage();
-}
-
-void ClientSession::handle(Msg3& msg)
-{
-    std::cout << "Received \"" << msg.doName() << "\" with ID=" << msg.doGetId() << '\n' <<
-        '\t' << msg.field_f1().name() << " = " << msg.field_f1().value() << '\n' <<
-               "\t\t= " << std::fixed << comms::units::getMicrometers<double>(msg.field_f1()) << " um\n" <<
-               "\t\t= " << std::fixed << comms::units::getMillimeters<double>(msg.field_f1()) << " mm\n" <<
-               "\t\t= " << std::fixed << comms::units::getCentimeters<double>(msg.field_f1()) << " cm\n" <<
-               "\t\t= " << std::fixed << comms::units::getMeters<double>(msg.field_f1()) << " m\n" <<
-        std::endl;
-
-    if (m_currentStage != CommsStage_Msg3) {
-        std::cerr << "ERROR: Unexpected message received: " << std::endl;
         return;
     }
 
@@ -161,7 +127,6 @@ void ClientSession::doNextStage()
     static const NextSendFunc Map[] = {
         /* CommsStage_Msg1 */ &ClientSession::sendMsg1,
         /* CommsStage_Msg2 */ &ClientSession::sendMsg2,
-        /* CommsStage_Msg3 */ &ClientSession::sendMsg3,
     };
     static const std::size_t MapSize = std::extent<decltype(Map)>::value;
     static_assert(MapSize == CommsStage_NumOfValues, "Invalid Map");
@@ -174,11 +139,6 @@ void ClientSession::sendMsg1()
 {
     Msg1 msg;
 
-    msg.field_f1().setScaled(1.234f);
-    assert(msg.field_f1().value() == 1234);
-
-    msg.field_f2().setScaled(12.345678);
-    assert(msg.field_f2().value() == 12345678);
 
     sendMessage(msg);
 }
@@ -187,17 +147,10 @@ void ClientSession::sendMsg2()
 {
     Msg2 msg;
 
-    comms::units::setMinutes(msg.field_f1(), 1.5);
-    comms::units::setMeters(msg.field_f2(), 1.2);
-
-    sendMessage(msg);
-}
-
-void ClientSession::sendMsg3()
-{
-    Msg3 msg;
-
-    comms::units::setMillimeters(msg.field_f1(), 123.45678);
+    std::cout << "Max length 4: " << Msg2::Field_f1::ValueType::value_type::Field_prop4::maxLength() << std::endl;
+    std::cout << "Max length 5: " << Msg2::Field_f1::ValueType::value_type::Field_prop5::maxLength() << std::endl;
+    std::cout << "Max length 6: " << Msg2::Field_f1::ValueType::value_type::Field_prop6::maxLength() << std::endl;
+    std::cout << "Max length: " << Msg2::Field_f1::ValueType::value_type::maxLength() << std::endl;
 
     sendMessage(msg);
 }
---
+++ tutorials/tutorial4/src/ServerSession.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "tutorial4/Message.h"
+#include "tutorial4/frame/Frame.h"
+
+#include "Session.h"
+
+namespace cc_tutorial
+{
+
+class ServerSession : public Session
+{
+    using Base = Session;
+public:
+    using Base::Base; // Inherit constructors
+
+    // Common interface class for all the messages
+    using Message =
+        tutorial4::Message<
+            comms::option::app::ReadIterator<const std::uint8_t*>, // Polymorphic read
+            comms::option::app::WriteIterator<std::uint8_t*>, // Polymorphic write
+            comms::option::app::LengthInfoInterface, // Polymorphic length calculation
+            comms::option::app::IdInfoInterface, // Polymorphic message ID retrieval
+            comms::option::app::NameInterface // Polymorphic message name retrieval
+        >;
+        
+    // Handle all the received messages in a single function
+    void handle(Message& msg);
+
+protected:
+    virtual std::size_t processInputImpl(const std::uint8_t* buf, std::size_t bufLen) override final;
+
+private:
+
+    using Frame = tutorial4::frame::Frame<Message>;
+
+    Frame m_frame;
+};
+
+} // namespace cc_tutorial
