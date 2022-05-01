77d22a435bf92c018d5d3e70979176fdbd160831
Alex Robenko <arobenko@gmail.com>
2022-05-01 13:53:43 +1000

    Testing with VS2022.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,7 +1,5 @@
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     set TOOLCHAIN=msvc14
-    set QT_SUBDIR=msvc2015
-    set QT_VER=5.6
     set BOOST_VER=1_65_1
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2015
@@ -12,8 +10,6 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     )
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
-    set QT_SUBDIR=msvc2017
-    set QT_VER=5.11
     set BOOST_VER=1_69_0
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
@@ -25,9 +21,7 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
     
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
     set TOOLCHAIN=msvc16
-    set QT_SUBDIR=msvc2017
-    set QT_VER=5.14
-    set BOOST_VER=1_71_0
+    set BOOST_VER=1_73_0
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
@@ -35,24 +29,19 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
         echo Performing amd64 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
     )
+) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2022" (
+    set TOOLCHAIN=msvc17
+    set BOOST_VER=1_77_0
+    IF "%PLATFORM%"=="x86" (
+        echo Performing x86 build in VS2022
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+    ) ELSE (
+        echo Performing amd64 build in VS2022
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )     
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
 )
-
-set QTDIR_PREFIX=C:/Qt/%QT_VER%
-IF "%PLATFORM%"=="x86" (
-    set QTDIR_SUFFIX=
-) ELSE (
-    set QTDIR_SUFFIX=_64
-)
-
-set QTDIR=%QTDIR_PREFIX%/%QT_SUBDIR%%QTDIR_SUFFIX%
-IF NOT EXIST %QTDIR% (
-    echo WARNING: %QTDIR% does not exist!!!
-    set QTDIR=%QTDIR_PREFIX%/msvc2015%QTDIR_SUFFIX%
-)
-
-echo Using Qt5 from %QTDIR%
 
 set BOOST_DIR=C:\Libraries\boost_%BOOST_VER%
